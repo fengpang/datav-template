26253efaf673d3400a5db0d57267645aeca27904
fengpang <fengpang@631663208@gmail.com>
2019-10-11 17:47:46 +0800

    简单高阶组件实现？？

--- src/plugins/chartWraper.js
+++ src/plugins/chartWraper.js
@@ -1,14 +1,16 @@
 import components from 'v-charts/lib/index.common'
 
-function componnetHandler (component) {
+/* 把v-charts的组件重新包装了一遍统一加上theme属性 避免每次添加theme属性，也可以在此对组件进行一些公共逻辑处理 */
+function wraper (component) {
   return {
     render(h) {
       return h('div',  {class: 'test'},
       [
-       h(component, {theme: this.$store.state.echartsTheme, ...this.$attrs})
+       h(component, {attrs: Object.assign(this.$attrs, {theme: this.$store.state.echartsTheme})})
       ])
     },
     created () {
+      /* do something */
     },
     name: component.name
   }
@@ -17,9 +19,8 @@ function componnetHandler (component) {
 const wrapedComponents = []
 
 for(let key in components) {
-  key !== 'install' && wrapedComponents.push(componnetHandler(components[key])) 
+  key !== 'install' && wrapedComponents.push(wraper(components[key])) 
 }
-console.log(wrapedComponents)
 export default function install (Vue) {
   wrapedComponents.forEach(component => {
     Vue.component(component.name, component)
